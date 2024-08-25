1b6f38aa9ade74bf195ae7351b9d312c84feb006
Wolfgang Rosner <wrosner@tirnet.de>
2024-08-26 00:17:40 +0200

    rearrangea and prepare for aux sensor pub

--- log_aux_ecowitt.pl
+++ log_aux_ecowitt.pl
@@ -12,7 +12,14 @@ use POSIX qw(strftime);	# time string formatting
 my $mqtt_server = "homeserver.rosner.lokal";
 my $ew_topic = "wetter/test";
 my $station = 1;
+
+my $aux_topic = "wetter/pleussen/aux";
+my $aux_dtime = 600 ; # max time in s between messages if nothing special happens
+my $aux_dtemp = 1   ; # temp diff in °C to trigger a message
+my $aux_dhum  = 2   ; # humidity diff in % to trigger a message
+
 my $db_creds ="my.cnf";
+
 my $debug = 3;
 
 # ======= eo config ============
@@ -42,6 +49,9 @@ my $dbh = DBI->connect($dsn, $user, $passwd)
 debug_print(1,  "\t...connected to database \n\n") ;
 # exit;
 
+#==============
+
+# __main__ MQTT subscription callback handler 
 $mqtt->run(
         $ew_topic => \&do_ecowitt,
         # "#" => \&parse_default,
@@ -53,11 +63,13 @@ $mqtt->run(
 
 sub noop {}
 
+# for dbug: we may parse other messages
 sub parse_default  {
             my ($topic, $message) = @_;
             print "default: [$topic] $message\n";
         }
 
+# MQTT callback distributor 
 sub do_ecowitt {
   my $hr = parse_ecowitt(@_);	
   debug_print(4, Dumper($hr)); 
@@ -67,15 +79,6 @@ sub do_ecowitt {
   # parse and process aux sensor data
   do_auxs($hr);
 
-  # my $auxs = parse_aux($hr);
-  # debug_print(3, Dumper($auxs));
-  # for my $sn (0 .. $#$auxs) {
-  #  next unless (defined($$auxs[$sn]));   
-  #  printf ("dummy do sensor number %d -> %s \n", $sn, 
-  #	 scalar(keys %{$$auxs[$sn]} )  ); 
-  #   log_aux( $hr->{'dateutc'}, $station, $sn,  $$auxs[$sn]  );
-  # }
-  # exit;
 }
 
 
@@ -98,8 +101,8 @@ sub do_auxs {
   for my $sn (0 .. $#$auxs) {
     next unless (defined($$auxs[$sn]));
 
-    printf "do sensor number %d -> %s \n", $sn,
-         scalar(keys %{$$auxs[$sn]} )  ;
+    debug_print(3, sprintf ("do sensor number %d -> %s \n", $sn,
+	         scalar(keys %{$$auxs[$sn]} ) ) ) ;
 
     # skip update if values haven't changed
     # "     ... checking sensor change .... \n";
@@ -107,8 +110,12 @@ sub do_auxs {
       next if eq_hashes($$auxs[$sn] , $$old_auxs[$sn]);
     }
     
-    print "       ... sensor changed, perform update\n";
+    debug_print(3, "       ... sensor changed, perform update\n");
     log_aux( $hr->{'dateutc'}, $station, $sn,  $$auxs[$sn]  );
+
+    debug_print(3, "       ... sensor changed, check pub conditions\n"); 
+    pub_aux( $hr->{'dateutc'}, $station, $sn,  $$auxs[$sn]  );
+
   }
 
   $old_auxs = $auxs ; # hope that assigning ref effectively clones
@@ -134,7 +141,7 @@ sub parse_aux {
 sub log_aux {
   # print (Dumper (\@_));
   my ($idx, $stat, $sn, $shr) = @_;
-  print (Dumper($idx, $stat, $sn, $shr));
+  debug_print(4, (Dumper($idx, $stat, $sn, $shr)));
 
   my $sql = "INSERT INTO `aux_th` SET";
   $sql .= sprintf (" \n `idx`        = '%s'   ", $idx);
@@ -148,13 +155,22 @@ sub log_aux {
 
   $sql .= " ;\n" ;
 
-  debug_print(3, $sql);
+  debug_print(4, $sql);
   # execute sql statement
   my $affected = $dbh->do($sql);
   debug_print (2, "\t$affected Datasets of sensors updated\n");
  
 }
 
+#  pub_aux( $hr->{'dateutc'}, $station, $sn,  $$auxs[$sn]  );
+sub pub_aux {
+  my ($idx, $stat, $sn, $shr) = @_;
+  debug_print(2, sprintf("--- pub_aux - station: %d, sensor: %d, utc-time: %s\n", $stat, $sn, $idx));
+  debug_print(3, Dumper($shr));
+
+}
+
+#----------------------
 
 sub log_ecowitt {
   my $sql = build_ew_SQL(@_[0]);
